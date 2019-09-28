56a2bfb32d2bd965e921ef5e977db917f44b66e2
looneytkp <sgm4kv@gmail.com>
2019-09-28 10:05:33 +0000

    nothing really

--- tmp.sh
+++ tmp.sh
@@ -2,14 +2,14 @@
 set -ex
 rm ~/.tmp/'movie list.txt' && touch ~/.tmp/'movie list.txt'
 COUNT=1; COUNT1=0
-echo '---script starts above this---' > 'movie list.txt'
+echo '---script starts above this---' > ~/.tmp/'movie list.txt'
 while true; do
     curl -s -o ~/.tmp/file.x -X GET "https://turbodl.xyz/wp-json/wp/v2/posts?page=$COUNT&per_page=100"
-    if grep -q 'rest_post_invalid_page_number' file.x; then break; fi
+    if grep -q 'rest_post_invalid_page_number' ~/.tmp/file.x; then break; fi
     while [ $COUNT1 -lt 100 ]; do
 		title=$(cat ~/.tmp/file.x | jq -r ".[$COUNT1].title.rendered" | sed -e "s/&#8211;/-/g; s/&#8217;/'/g; s/&#038;/\&/g; s/&#8216;/'/g; s/&#822[0-1];/\"/g; s/&amp;/\&/g; s/ [UPDATED]//")
         if [ "$title" == null ]; then break; fi
-        echo "$title" >> 'movie list.txt'
+        echo "$title" >> ~/.tmp/'movie list.txt'
         COUNT1=$((COUNT1+1))
     done
     COUNT1=0; COUNT=$((COUNT+1))
