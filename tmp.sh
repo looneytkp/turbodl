fe9bcadcb3826ea13875f4b1e70cdaf36646384b
looneytkp <sgm4kv@gmail.com>
2019-09-28 14:56:31 +0000

    fix naming error

--- tmp.sh
+++ tmp.sh
@@ -6,7 +6,7 @@ while true; do
     curl -s -o ~/.tmp/file.x -X GET "https://turbodl.xyz/wp-json/wp/v2/posts?page=$COUNT&per_page=100"
     if grep -q 'rest_post_invalid_page_number' ~/.tmp/file.x; then break; fi
     while [ $COUNT1 -lt 100 ]; do
-		title=$(cat ~/.tmp/file.x | jq -r ".[$COUNT1].title.rendered" | sed -e "s/&#8211;/-/g; s/&#8217;/'/g; s/&#038;/\&/g; s/&#8216;/'/g; s/&#822[0-1];/\"/g; s/&amp;/\&/g; s/ [UPDATED]//")
+		title=$(cat ~/.tmp/file.x | jq -r ".[$COUNT1].title.rendered" | sed -e "s/&#8211;/-/g; s/&#8217;/'/g; s/&#038;/\&/g; s/&#8216;/'/g; s/&#8220;/\"/g; s/&#8221;/\"/g; s/&amp;/\&/g; s/ [UPDATED]//")
         if [ "$title" == null ]; then break; fi
         echo "$title" >> ~/.tmp/'movie list.txt'
         COUNT1=$((COUNT1+1))
